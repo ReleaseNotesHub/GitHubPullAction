aba4f94455c4060499321629d090b5e344cc260f
Patrick <patrick.nolan@thinkabout.com.au>
2021-10-28 13:20:08 +1000

    Initial Check-in of core logic

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # GitHubPullAction
-ReleaseNotesHub for GitHub Pull Action Trigger
+This action will trigger ReleaseNotesHub to generate create notes for the given project and version.
+
+## Helpful links
+- https://support.releasenoteshub.com/article/show/126524-how-do-i-setup-a-connection-for-github
+- https://support.releasenoteshub.com/article/show/126516-how-to-configure-a-project-query-for-github
+- https://support.releasenoteshub.com/article/show/108978-how-do-i-trigger-a-pull-request-for-github
+
+## Inputs
+
+## `rnh-apikey`
+
+**Required** Your ReleaseNotesHub API Key.
+
+## `rnh-projectid`
+
+**Required** The Unique Identifier (GUID) for your ReleaseNotesHub Project.
+
+## `rnh-release-version`
+
+**Required** The release version .e.g. 12.11.5.1, 12.11.5-beta.
+
+## `rnh-release-name`
+
+The release name.
+
+## `rnh-release-description`
+
+The release description.
+
+## `rnh-release-publish`
+
+Set release to Published. Default `"false"`.
+
+## `rnh-release-ignoreifexists`
+
+If release exists in ReleaseNotesHub and value is true, then do nothing, otherwise update release. Default `"false"`.
+
+## `rnh-release-createonnotfound`
+
+If release does not exist in ReleaseNotesHub and value is true, then create release, otherwise do nothing. Default `"true"`.
+
+
+## Example usage
+
+uses: actions/GitHubPullAction@v1.1
+with:
+  rnh-apikey: 'c977da13-149c-4f4b-b5d5-bd7d1491853d'
+  rnh-projectid: '42d3a80d-b9b7-4998-bef0-1af9f79dfd4c'  
+  rnh-release-version: '12.11.5.1'
+  rnh-release-publish: 'true'          
---
+++ index.js
@@ -0,0 +1,63 @@
+import fetch from 'node-fetch';
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+/**
+ * @typedef GenerationOptions
+ * @type {object}
+ * @property {string} [RnhApiKey]
+ * @property {string} [ProjectId]
+ * @property {string} [ReleaseVersion] 
+ * @property {string} [ReleaseName]
+ * @property {string} [ReleaseDescription]
+ * @property {boolean} [Publish]
+ * @property {boolean} [IgnoreIfExists]
+ * @property {boolean} [CreateOnNotFound]
+ */
+
+ function readString(key) {
+    return core.getInput(key);
+  }
+  
+  function readBoolean(key) {
+    const val = readString(key);
+    return val === true || val === 'true';
+  }
+  
+  /** @type {GenerationOptions} */
+  const options = {
+    RnhApiKey: readString('rnh-apikey'),
+    ProjectId: readString('rnh-projectid'),    
+    ReleaseVersion: readString('rnh-release-version'),
+    ReleaseName: readString('rnh-release-name'),
+    ReleaseDescription: readString('rnh-release-description'),
+    Publish: readBoolean('rnh-release-publish'),
+    IgnoreIfExists: readBoolean('rnh-release-ignoreifexists'),
+    CreateOnNotFound: readBoolean('rnh-release-createonnotfound'),
+  };
+
+try {
+  console.log(`ReleaseNotesHub Pull Request Triggered`);
+
+  var body = {
+      'version': '' + options.ReleaseVersion + '',
+      'name': '' +  options.ReleaseName + '',
+      'description': '' +  options.ReleaseDescription + '',
+      'publish': '' +  options.Publish + '',
+      'ignoreIfExists': '' +  options.IgnoreIfExists + '',    
+      'createOnNotFound': '' +  options.CreateOnNotFound + ''                
+    };
+
+  var response = await fetch('https://api.releasenoteshub.com/api/pull/PullVersion/' + options.ProjectId, {
+      method: 'post',
+      body: JSON.stringify(body),
+      headers: {'Content-Type': 'application/json',
+      'Accept': 'application/json',
+      'Authorization': 'ApiKey ' + options.RnhApiKey}
+  });
+  var data = await response.stringify();
+  console.log(data);  
+
+} catch (error) {
+  core.setFailed(error.message);
+}
